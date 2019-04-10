b5a43e4510d915595ac15a662408051075e51135
panteng <panteng@xiaomi.com>
2019-04-10 20:17:36 +0800

    seq2seq 加入 attention 机制

--- src/tf/Seq2Seq/BaseSeq2Seq.py
+++ src/tf/Seq2Seq/BaseSeq2Seq.py
@@ -128,6 +128,49 @@ class Seq2Seq_2LSTM_2LSTM_Model2(tf.keras.Model):
         return self.dense(decoder_lstm2)
 
 
+class Seq2Seq_2LSTM_2LSTM_Attention_Model(tf.keras.Model):
+    """
+    编码器&解码器采用两层LSTM
+    加入Attention机制，效果提升很明显
+    """
+
+    def __init__(self):
+        super(Seq2Seq_2LSTM_2LSTM_Attention_Model, self).__init__()
+        self.embedding = tf.keras.layers.Embedding(vocabulary_size, embedding_size)
+        self.encoder1 = tf.keras.layers.LSTM(units=128, return_sequences=True, return_state=True)
+        self.encoder2 = tf.keras.layers.LSTM(units=128, return_sequences=True, return_state=True)
+
+        self.decoder_embedding = tf.keras.layers.Embedding(tag_size, embedding_size)
+        self.attention = tf.Variable(tf.ones(shape=[seq_length, 128]))
+        self.decoder1 = tf.keras.layers.LSTM(units=128, return_sequences=True)
+        self.decoder2 = tf.keras.layers.LSTM(units=128, return_sequences=True)
+        self.dense = tf.keras.layers.Dense(units=tag_size)
+
+    def __call__(self, inputs, seq_label, is_train=True):
+        embedded = self.embedding(inputs)
+        encoder_lstm1 = self.encoder1(embedded)
+        encoder_lstm2 = self.encoder2(encoder_lstm1[0])
+
+        state1_h = encoder_lstm1[1]
+        state1_c = encoder_lstm1[2]
+        state2_h = encoder_lstm2[1]
+        state2_c = encoder_lstm2[2]
+        encoder_outseq = encoder_lstm2[0]
+        attention_input = tf.multiply(encoder_outseq, self.attention)
+
+        if is_train:
+            start = np.ones(shape=[batch_size, 1]) * 7
+            decoder_input = tf.convert_to_tensor(np.concatenate((start, seq_label), axis=1)[:, :-1])
+        else:
+            decoder_input = tf.convert_to_tensor(seq_label)
+
+        decoder_embeded = self.decoder_embedding(decoder_input)
+        input_with_attention = tf.concat((decoder_embeded, attention_input[:, :seq_label.shape[1]]), axis=2)
+        decoder_lstm1 = self.decoder1(input_with_attention, initial_state=[state1_h, state1_c])
+        decoder_lstm2 = self.decoder2(decoder_lstm1, initial_state=[state2_h, state2_c])
+        return self.dense(decoder_lstm2)
+
+
 def get_data(file_path):
     with open(file_path) as file:
         seq1 = []
@@ -152,7 +195,7 @@ def loss_function(real, preds):
 
 data_iter = get_data("../../../data/ner/data.txt")
 
-model = Seq2Seq_2LSTM_2LSTM_Model2()
+model = Seq2Seq_2LSTM_2LSTM_Attention_Model()
 EPOCHS = 10000
 optimizer = tf.train.AdamOptimizer()
 for epoch in range(EPOCHS):
@@ -163,19 +206,21 @@ for epoch in range(EPOCHS):
             predictions = model(seq1, seq2)
             loss = loss_function(seq2, predictions)
             print(loss)
-            if epoch % 10 == 0:
+            if epoch % 1 == 0:
                 pre_lables = tf.argmax(predictions, axis=2)
-                print(np.sum(np.sum(pre_lables.numpy() == seq2, axis=1) > 297))
+                print(np.average(np.sum(pre_lables.numpy() == seq2, axis=1)))
             grads = tape.gradient(loss, model.variables)
             optimizer.apply_gradients(zip(grads, model.variables))
     except StopIteration as e:
         data_iter = get_data("../../../data/ner/data.txt")
         # Test
+        """很耗时，所以暂时注释掉
         seq1, seq2 = data_iter.__next__()
         seq1 = tf.convert_to_tensor(seq1, dtype=tf.int32)
         start_input = np.ones([batch_size, 1]) * 7
         pre_lables = None
         for i in range(seq_length):
+            print(i)
             if pre_lables is None:
                 input = start_input
             else:
@@ -184,3 +229,4 @@ for epoch in range(EPOCHS):
             pre_lables = tf.argmax(predictions, axis=2)
         print(np.sum(pre_lables.numpy() == seq2, axis=1))
         print("one epoch complete... ...")
+        """

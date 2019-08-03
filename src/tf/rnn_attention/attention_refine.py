18d7bd7837d70e053e34c9a0f90265cad5f7f510
panteng <panteng@xiaomi.com>
2019-08-03 15:09:11 +0800

    encoder - decoder 多层，使用翻译语料测试；训练集 完全准确率96%

--- src/tf/rnn_attention/attention_refine.py
+++ src/tf/rnn_attention/attention_refine.py
@@ -1,7 +1,34 @@
 import tensorflow as tf
 import numpy as np
 
-print(tf.__version__)
+EMBEDDING_SIZE = 64
+DIC_SIZE = 7148
+LABEL_SIZE = 3440  # 0为padding，24为start
+SEQ_LEN = 128
+BATCH_SIZE = 512
+GRU_UNITS = 256
+model_path = "./"
+
+feature_description = {
+    "word_id": tf.io.VarLenFeature(dtype=tf.int64),
+    "word_label": tf.io.VarLenFeature(dtype=tf.int64)
+}
+
+
+def parse_fn(example_proto):
+    example = tf.io.parse_single_example(example_proto, feature_description)
+    f = tf.sparse.to_dense(example['word_id'])
+    l = tf.sparse.to_dense(example['word_label'])
+    return tf.pad(f, [[0, SEQ_LEN - tf.shape(f)[0]]]), tf.pad(l, [[0, SEQ_LEN - tf.shape(l)[0]]]), tf.shape(f)[0], \
+           tf.shape(l)[0]
+
+
+def input_fn(file_path):
+    ds = tf.data.TFRecordDataset(file_path, num_parallel_reads=4) \
+        .map(parse_fn, num_parallel_calls=4) \
+        .shuffle(buffer_size=1024) \
+        .batch(batch_size=BATCH_SIZE, drop_remainder=True).prefetch(51200).repeat()
+    return ds
 
 
 class GruCell:
@@ -11,10 +38,10 @@ class GruCell:
         :param step_dimension: 每一时间步的输入维度
         """
         self.units = units
-        self.en_w_r_z = tf.Variable(tf.truncated_normal(shape=[step_dimension + self.units, self.units * 2]))
-        self.en_b_r_z = tf.Variable(tf.truncated_normal(shape=[units * 2, ]))
-        self.en_w_h = tf.Variable(tf.truncated_normal(shape=[step_dimension + self.units, self.units]))
-        self.en_b_h = tf.Variable(tf.truncated_normal(shape=[units, ]))
+        self.en_w_r_z = tf.Variable(tf.truncated_normal(shape=[step_dimension + self.units, self.units * 2]) / 10000)
+        self.en_b_r_z = tf.Variable(tf.truncated_normal(shape=[units * 2, ]) / 10000)
+        self.en_w_h = tf.Variable(tf.truncated_normal(shape=[step_dimension + self.units, self.units]) / 10000)
+        self.en_b_h = tf.Variable(tf.truncated_normal(shape=[units, ]) / 10000)
 
     def en_cond(self, i, en_embeded, en_gru_output):
         return i < tf.shape(en_embeded)[1]
@@ -54,10 +81,11 @@ class GruCellAttentionDecoder:
         :param step_dimension: 每一时间步的输入维度
         """
         self.units = units
-        self.de_w_r_z = tf.Variable(tf.truncated_normal(shape=[step_dimension + self.units * 2, self.units * 2]))
-        self.de_b_r_z = tf.Variable(tf.truncated_normal(shape=[self.units * 2, ]))
-        self.de_w_h = tf.Variable(tf.truncated_normal(shape=[step_dimension + self.units * 2, self.units]))
-        self.de_b_h = tf.Variable(tf.truncated_normal(shape=[self.units, ]))
+        self.de_w_r_z = tf.Variable(
+            tf.truncated_normal(shape=[step_dimension + self.units * 2, self.units * 2]) / 10000)
+        self.de_b_r_z = tf.Variable(tf.truncated_normal(shape=[self.units * 2, ]) / 10000)
+        self.de_w_h = tf.Variable(tf.truncated_normal(shape=[step_dimension + self.units * 2, self.units]) / 10000)
+        self.de_b_h = tf.Variable(tf.truncated_normal(shape=[self.units, ]) / 10000)
 
     def de_cond(self, i, de_embeded, de_gru_output, encoder_output):
         return i < tf.shape(de_embeded)[1]
@@ -65,7 +93,6 @@ class GruCellAttentionDecoder:
     def de_gru(self, i, de_embeded, de_gru_output, encoder_output):
         step_in = de_embeded[:, i]
         last_state = de_gru_output[:, i]
-        # 由于使用了tf.squeeze，所以在这里batch_size=1或者seq len=1时，可能会有问题
         attention_weight = tf.nn.softmax(tf.squeeze(tf.matmul(encoder_output, tf.expand_dims(last_state, axis=2))))
         context_c = tf.reduce_sum(tf.multiply(tf.expand_dims(attention_weight, axis=2), encoder_output), axis=1)
         step_in = tf.concat((step_in, context_c), axis=-1)
@@ -95,52 +122,115 @@ class GruCellAttentionDecoder:
         return decoder_output
 
 
-input_data = np.array([[1, 2, 3, 4, 5], [2, 4, 1, 4, 5], [4, 3, 2, 3, 1]])  # batch_size=3,seq_length=5
-input_label = np.array([[5, 4, 3, 2, 1], [4, 2, 5, 2, 1], [2, 3, 4, 3, 5]])
-
-input_vocab_size = 6  # 从0开始，0可以认为padding
-output_vocab_size = 7  # 需要考虑解码时的start，暂定6为start，0为padding
-batch_size = 3
-seq_len = 5
-embedding_size = 4
-gru_units = 4
 sess = tf.Session()
 # 编码
 en_input = tf.placeholder(tf.int32, shape=[None, None])
-en_embedding_variable = tf.Variable(tf.truncated_normal(shape=[input_vocab_size, embedding_size]))
+en_embedding_variable = tf.Variable(tf.truncated_normal(shape=[DIC_SIZE, EMBEDDING_SIZE]))
 en_embeded = tf.nn.embedding_lookup(en_embedding_variable, en_input)
-en_gru_cell = GruCell(units=gru_units, step_dimension=embedding_size)
-gru_init_state = tf.zeros(shape=[batch_size, 1, en_gru_cell.units])  # 这里不应该定义称为变量
-encoder_output = en_gru_cell(en_embeded, gru_init_state)
+en_gru_cell_1 = GruCell(units=GRU_UNITS, step_dimension=EMBEDDING_SIZE)
+gru_init_state_1 = tf.zeros(shape=[BATCH_SIZE, 1, en_gru_cell_1.units])  # 这里不应该定义称为变量
+encoder_output_1 = en_gru_cell_1(en_embeded, gru_init_state_1)
+
+en_gru_cell_2 = GruCell(units=GRU_UNITS, step_dimension=en_gru_cell_1.units)
+gru_init_state_2 = tf.zeros(shape=[BATCH_SIZE, 1, en_gru_cell_2.units])  # 这里不应该定义称为变量
+encoder_output_2 = en_gru_cell_2(encoder_output_1[:, 1:], gru_init_state_2)
 
 # 解码
 de_in_label = tf.placeholder(tf.int32, shape=[None, None])  # batch_size,seq_len
-de_embedding_variable = tf.Variable(tf.truncated_normal(shape=[output_vocab_size, embedding_size]))
+de_embedding_variable = tf.Variable(tf.truncated_normal(shape=[LABEL_SIZE, EMBEDDING_SIZE]))
 de_embeded = tf.nn.embedding_lookup(de_embedding_variable, de_in_label)
-de_gru_cell = GruCellAttentionDecoder(gru_units, step_dimension=embedding_size)
-de_init_state = tf.expand_dims(encoder_output[:, -1], axis=1)  # init state
-decoder_output = de_gru_cell(de_embeded, de_init_state, encoder_output)
+de_gru_cell_1 = GruCellAttentionDecoder(GRU_UNITS, step_dimension=EMBEDDING_SIZE)
+de_init_state_1 = tf.expand_dims(encoder_output_1[:, -1], axis=1)  # init state
+decoder_output_1 = de_gru_cell_1(de_embeded, de_init_state_1, encoder_output_2[:, 1:])
+
+de_gru_cell_2 = GruCell(GRU_UNITS, step_dimension=de_gru_cell_1.units)
+de_init_state_2 = tf.expand_dims(encoder_output_2[:, -1], axis=1)  # init state
+decoder_output_2 = de_gru_cell_2(decoder_output_1[:, 1:], de_init_state_2)
+
 # 全连接
-dense_w = tf.Variable(tf.truncated_normal(shape=[gru_units, output_vocab_size]))
-dense_b = tf.Variable(tf.truncated_normal(shape=[output_vocab_size, ]))
-output = tf.tensordot(decoder_output, dense_w, [[2], [0]]) + dense_b
-# 易错点，labels=de_in_label[:, 1:] 应该从第1个元素开始，而不是第0个
+dense_w_1 = tf.Variable(tf.truncated_normal(shape=[GRU_UNITS, GRU_UNITS // 2]))
+dense_b_1 = tf.Variable(tf.truncated_normal(shape=[GRU_UNITS // 2, ]))
+dense_w_2 = tf.Variable(tf.truncated_normal(shape=[GRU_UNITS // 2, LABEL_SIZE]))
+dense_b_2 = tf.Variable(tf.truncated_normal(shape=[LABEL_SIZE, ]))
+
+dense_1 = tf.nn.leaky_relu(tf.tensordot(decoder_output_2, dense_w_1, [[2], [0]]) + dense_b_1)
+output = tf.nn.leaky_relu(tf.tensordot(dense_1, dense_w_2, [[2], [0]]) + dense_b_2)
+
 loss = tf.losses.sparse_softmax_cross_entropy(labels=de_in_label[:, 1:], logits=output[:, 1:-1])
 
-optimizer = tf.train.RMSPropOptimizer(learning_rate=0.01).minimize(loss)
-decoder_start = np.zeros(shape=[batch_size, 1]) + output_vocab_size - 1
+optimizer = tf.train.AdamOptimizer(learning_rate=0.002).minimize(loss)
+decoder_start = np.zeros(shape=[BATCH_SIZE, 1]) + LABEL_SIZE - 1
+
 saver = tf.train.Saver()
+ds = input_fn("../../../data/translate/train_data.tfrecord").make_one_shot_iterator().get_next()
 # sess.run(tf.global_variables_initializer())
-saver.restore(sess, "./test.model")
-"""
-for i in range(1000):
-    #这里丢弃了input_label最后一个时间步，所以识别时不会识别最后一个时间步,实际应用中可以使用padding或者结束符来补上最后一个时间步
-    print(sess.run((loss, optimizer),
-                   feed_dict={en_input: input_data,
-                              de_in_label: np.concatenate((decoder_start, input_label), axis=-1)[:, 0:-1]}))
-saver.save(sess, save_path="./test.model")
-"""
-output = sess.run(output,
-                  feed_dict={en_input: input_data,
-                             de_in_label: np.concatenate((decoder_start, input_label), axis=-1)[:, 0:-1]})
-print(np.argmax(output, axis=2)[:, 1:-1])
+
+
+saver.restore(sess, save_path=model_path + "ner.model-1")
+
+
+def save_model():
+    np.savetxt("./params_v3/en_embeding", sess.run(en_embedding_variable))
+    np.savetxt("./params_v3/de_embedding", sess.run(de_embedding_variable))
+    np.savetxt("./params_v3/en_grucell_1_w_r_z", sess.run(en_gru_cell_1.en_w_r_z))
+    np.savetxt("./params_v3/en_grucell_1_b_r_z", sess.run(en_gru_cell_1.en_b_r_z))
+    np.savetxt("./params_v3/en_grucell_1_w_h", sess.run(en_gru_cell_1.en_w_h))
+    np.savetxt("./params_v3/en_grucell_1_b_h", sess.run(en_gru_cell_1.en_b_h))
+    # --
+    np.savetxt("./params_v3/en_grucell_2_w_r_z", sess.run(en_gru_cell_2.en_w_r_z))
+    np.savetxt("./params_v3/en_grucell_2_b_r_z", sess.run(en_gru_cell_2.en_b_r_z))
+    np.savetxt("./params_v3/en_grucell_2_w_h", sess.run(en_gru_cell_2.en_w_h))
+    np.savetxt("./params_v3/en_grucell_2_b_h", sess.run(en_gru_cell_2.en_b_h))
+    # --
+    np.savetxt("./params_v3/de_grucell_1_w_r_z", sess.run(de_gru_cell_1.de_w_r_z))
+    np.savetxt("./params_v3/de_grucell_1_b_r_z", sess.run(de_gru_cell_1.de_b_r_z))
+    np.savetxt("./params_v3/de_grucell_1_w_h", sess.run(de_gru_cell_1.de_w_h))
+    np.savetxt("./params_v3/de_grucell_1_b_h", sess.run(de_gru_cell_1.de_b_h))
+
+    np.savetxt("./params_v3/de_grucell_2_w_r_z", sess.run(de_gru_cell_2.en_w_r_z))
+    np.savetxt("./params_v3/de_grucell_2_b_r_z", sess.run(de_gru_cell_2.en_b_r_z))
+    np.savetxt("./params_v3/de_grucell_2_w_h", sess.run(de_gru_cell_2.en_w_h))
+    np.savetxt("./params_v3/de_grucell_2_b_h", sess.run(de_gru_cell_2.en_b_h))
+
+    np.savetxt("./params_v3/dense_w_1", sess.run(dense_w_1))
+    np.savetxt("./params_v3/dense_b_1", sess.run(dense_b_1))
+
+    np.savetxt("./params_v3/dense_w_2", sess.run(dense_w_2))
+    np.savetxt("./params_v3/dense_b_2", sess.run(dense_b_2))
+
+
+min_v = 0.1
+for i in range(100000):
+    features, labels, f_lengths, l_lengths = sess.run(ds)
+    """
+    这里在训练的时候不同批次的最大长度是不一样的，目的是为了加快训练速度，但对精度会有影响，
+    通过翻译场景来看，在预测时，适当的增加padding，对最终效果有一定的正面影响;
+    后续需要优化的地方是重新组织训练数据，比如长度小于10的分到一个批次，padding长度为10； 10 ～ 20的分到一个批次，padding长度为20
+    如果使用固定长度的padding训练，那么对算力的需求可能会增长几十倍
+    """
+    f_max_len = f_lengths[np.argmax(f_lengths, axis=-1)]
+    l_max_len = l_lengths[np.argmax(l_lengths, axis=-1)]
+
+    loss_value, _ = sess.run((loss, optimizer), feed_dict={en_input: features[:, :f_max_len + 1],
+                                                           de_in_label: np.concatenate(
+                                                               (decoder_start, labels[:, :l_max_len + 1]),
+                                                               axis=-1)[:,
+                                                                        0:-1]})
+
+    print(i, loss_value)
+    if i % 10 == 0 and (min_v > loss_value):
+        saver.save(sess, model_path + "ner.model", global_step=int(loss_value * 1000))
+        min_v = loss_value
+
+    """
+    if i % 1 == 0:
+        # save_model()
+        # saver.save(sess, model_path + "ner.model", global_step=int(loss_value * 1000))
+
+        pred = sess.run(output,
+                        feed_dict={en_input: features[:, :f_max_len + 1],
+                                   de_in_label: np.concatenate((decoder_start, labels[:, :l_max_len + 1]),
+                                                               axis=-1)[:, 0:-1]})
+        print(i, np.sum(np.sum(np.argmax(pred, axis=2)[:, 1:-1] == labels[:, :l_max_len], axis=1) == l_max_len))
+    """
+sess.close()
--- src/tf/rnn_attention/numpy_gru_attention.py
+++ src/tf/rnn_attention/numpy_gru_attention.py
@@ -1,19 +1,29 @@
-import math
-
 import numpy as np
+import tensorflow as tf
+import math
 
 en_embeding = np.loadtxt("./params_v3/en_embeding")
 de_embeding = np.loadtxt("./params_v3/de_embedding")
-en_gru_w_r_z = np.loadtxt("./params_v3/en_grucell_w_r_z")
-en_gru_b_r_z = np.loadtxt("./params_v3/en_grucell_b_r_z")
-en_gru_w_h = np.loadtxt("./params_v3/en_grucell_w_h")
-en_gru_b_h = np.loadtxt("./params_v3/en_grucell_b_h")
-de_gru_w_r_z = np.loadtxt("./params_v3/de_grucell_w_r_z")
-de_gru_b_r_z = np.loadtxt("./params_v3/de_grucell_b_r_z")
-de_gru_w_h = np.loadtxt("./params_v3/de_grucell_w_h")
-de_gru_b_h = np.loadtxt("./params_v3/de_grucell_b_h")
-dense_w = np.loadtxt("./params_v3/dense_w")
-dense_b = np.loadtxt("./params_v3/dense_b")
+en_gru_1_w_r_z = np.loadtxt("./params_v3/en_grucell_1_w_r_z")
+en_gru_1_b_r_z = np.loadtxt("./params_v3/en_grucell_1_b_r_z")
+en_gru_1_w_h = np.loadtxt("./params_v3/en_grucell_1_w_h")
+en_gru_1_b_h = np.loadtxt("./params_v3/en_grucell_1_b_h")
+en_gru_2_w_r_z = np.loadtxt("./params_v3/en_grucell_2_w_r_z")
+en_gru_2_b_r_z = np.loadtxt("./params_v3/en_grucell_2_b_r_z")
+en_gru_2_w_h = np.loadtxt("./params_v3/en_grucell_2_w_h")
+en_gru_2_b_h = np.loadtxt("./params_v3/en_grucell_2_b_h")
+de_gru_1_w_r_z = np.loadtxt("./params_v3/de_grucell_1_w_r_z")
+de_gru_1_b_r_z = np.loadtxt("./params_v3/de_grucell_1_b_r_z")
+de_gru_1_w_h = np.loadtxt("./params_v3/de_grucell_1_w_h")
+de_gru_1_b_h = np.loadtxt("./params_v3/de_grucell_1_b_h")
+de_gru_2_w_r_z = np.loadtxt("./params_v3/de_grucell_2_w_r_z")
+de_gru_2_b_r_z = np.loadtxt("./params_v3/de_grucell_2_b_r_z")
+de_gru_2_w_h = np.loadtxt("./params_v3/de_grucell_2_w_h")
+de_gru_2_b_h = np.loadtxt("./params_v3/de_grucell_2_b_h")
+dense_w_1 = np.loadtxt("./params_v3/dense_w_1")
+dense_b_1 = np.loadtxt("./params_v3/dense_b_1")
+dense_w_2 = np.loadtxt("./params_v3/dense_w_2")
+dense_b_2 = np.loadtxt("./params_v3/dense_b_2")
 
 
 def sigmoid(x):
@@ -24,6 +34,10 @@ def softmax(x):
     exp_x = np.exp(x)
     softmax_x = exp_x / np.sum(exp_x)
     return softmax_x
+
+
+def leaky_relu(x):
+    return np.where(x > 0, x, x * 0.2)
 
 
 class GruCell:
@@ -39,9 +53,13 @@ class GruCell:
         self.units = self.w_h.shape[1]
         self.step_d = self.w_h.shape[0] - self.w_h.shape[1]
 
-    def __call__(self, seqs, *args, **kwargs):
+    def __call__(self, seqs, last_state=None, *args, **kwargs):
+        if last_state is None:
+            last_state = np.zeros(shape=(self.units,))
+        if np.ndim(seqs) == 1:
+            seqs = np.expand_dims(seqs, axis=0)
+
         seq_len = seqs.shape[0]
-        last_state = np.zeros(shape=(self.units,))
         en_gru_output = [last_state]
         for i in range(seq_len):
             step_in = seqs[i]
@@ -76,26 +94,53 @@ class GruCellAttentionDecoder():
         return h
 
 
-en_gru_cell = GruCell(en_gru_w_r_z, en_gru_b_r_z, en_gru_w_h, en_gru_b_h)
-decoder = GruCellAttentionDecoder(de_gru_w_r_z, de_gru_b_r_z, de_gru_w_h, de_gru_b_h)
+en_gru_cell_1 = GruCell(en_gru_1_w_r_z, en_gru_1_b_r_z, en_gru_1_w_h, en_gru_1_b_h)
+en_gru_cell_2 = GruCell(en_gru_2_w_r_z, en_gru_2_b_r_z, en_gru_2_w_h, en_gru_2_b_h)
+decoder_1 = GruCellAttentionDecoder(de_gru_1_w_r_z, de_gru_1_b_r_z, de_gru_1_w_h, de_gru_1_b_h)
+decoder_2 = GruCell(de_gru_2_w_r_z, de_gru_2_b_r_z, de_gru_2_w_h, de_gru_2_b_h)
 
 
-def predict(ids):
+def predict(ids, decoder_len=None):
+    if decoder_len is None:
+        decoder_len = len(ids[0])
     en_embeded = en_embeding[ids]
-    encoder_vec = en_gru_cell(en_embeded)
-    init_state = encoder_vec[-1]
-    firs_in = de_embeding[6]
+    encoder_vec1 = en_gru_cell_1(en_embeded)
+    encoder_vec2 = en_gru_cell_2(encoder_vec1[1:])
+    init_state_1 = encoder_vec1[-1]
+    init_state_2 = encoder_vec2[-1]
+    firs_in = de_embeding[3439]
     ret_ids = []
-    for i in range(len(ids[0])):
-        state = decoder(firs_in, init_state, encoder_vec)
-        out = np.dot(np.reshape(state, (1, 4)), dense_w) + dense_b
-        firs_in = np.argmax(out, -1)
-        ret_ids.append(firs_in[0])
-        firs_in = de_embeding[firs_in[0]]
-        init_state = state
+    for i in range(decoder_len):
+        state_1 = decoder_1(firs_in, init_state_1, encoder_vec2[1:])
+        state_2 = decoder_2(state_1, init_state_2)[1]
+        dense_1 = leaky_relu(np.dot(np.reshape(state_2, (1, decoder_2.units)), dense_w_1) + dense_b_1)
+        dense_2 = leaky_relu(np.dot(np.reshape(dense_1, (1, dense_1.size)), dense_w_2) + dense_b_2)
+        firs_in = np.argmax(dense_2, -1)[0]
+        ret_ids.append(firs_in)
+        firs_in = de_embeding[firs_in]
+        init_state_1 = state_1
+        init_state_2 = state_2
     return ret_ids
 
 
 if __name__ == '__main__':
-    decoder_out = predict([[2, 4, 1, 4, 5]])
-    print(decoder_out)
+    with open("/home/panteng/PycharmProjects/sms-ner-py/data/translate/train_data.txt") as file:
+        t_count = 0
+        f_count = 0
+        for line in file:
+            f, l = line.strip("\n").split("\t")
+            f = list(map(lambda x: int(x), f.split(",")))
+            for i in range(10):
+                f.append(0)
+            l = list(map(lambda x: int(x), l.split(",")))
+            for i in range(10):
+                l.append(0)
+
+            pre_seqs = predict([f], decoder_len=len(l))
+            if pre_seqs == l:
+                t_count += 1
+            else:
+                f_count += 1
+            if t_count % 1000 == 0:
+                print(t_count, f_count)
+        print(t_count, f_count)
